6df7573202ad8403da4ba76b320b3cd7584c7f1a
ян козыренко <kopn48@mail.ru>
2020-10-04 17:24:12 +0300

    delete get query

--- test_react/ClientApp/src/components/MainContent/CurrentGame/CurrentGame.jsx
+++ test_react/ClientApp/src/components/MainContent/CurrentGame/CurrentGame.jsx
@@ -2,14 +2,8 @@ import React from 'react';
 import Autorization from './Autorization/Autorization';
 import classes from './CurrentGame.module.css';
 import $ from "jquery";
-import LogoSide from '../../Header/LogoSide/LogoSide';
 
 const CurrentGame = (props) => {
-    var result = $.get({
-        url: "/getAuthKey",
-        success: e => console.log(e)
-    });
-    
     return (
         <div className={classes.currentGameWrapper}>
             <span className={classes.total}>Игра#{props.total}</span>
